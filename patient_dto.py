149235a0ff1f0aad45f0e9fe0f3a67d274038b1a
Marco Vela <mvelar@uni.pe>
2021-12-11 15:51:19 -0500

    feat: enum fields
    Added validation for fields in request.

--- patient_dto.py
+++ patient_dto.py
@@ -1,20 +1,54 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, conint, confloat
+
+
+class SexEnum(Enum):
+    M = 'M'
+    F = 'F'
+
+
+class ChesPainTypeEnum(Enum):
+    TypicalAngina = 'TA'
+    AtypicalAngina = 'ATA'
+    NonAnginalPain = 'NAP'
+    Asymptomatic = 'ASY'
+
+
+class FastingBsEnum(IntEnum):
+    T = 1
+    F = 0
+
+
+class RestingECGEnum(Enum):
+    Normal = 'Normal'
+    ST = 'ST'
+    LVH = 'LVH'
+
+
+class ExerciseAnginaEnum(Enum):
+    Yes = 'Y'
+    No = 'N'
+
+
+class STSlopeEnum(Enum):
+    Up = 'Up'
+    Flat = 'Flat'
+    Down = 'Down'
 
 
 class Patient(BaseModel):
-    age: int
-    sex: str
-    chestPainType: str
-    restingBP: int
-    cholesterol: int
-    fastingBS: int
-    restingECG: str
-    maxHR: int
-    exerciseAngina: str
+    age: conint(gt=0)
+    sex: SexEnum
+    chestPainType: ChesPainTypeEnum
+    restingBP: confloat(ge=0.0)
+    cholesterol: confloat(ge=0.0)
+    fastingBS: FastingBsEnum
+    restingECG: RestingECGEnum
+    maxHR: conint(ge=60, le=202)
+    exerciseAngina: ExerciseAnginaEnum
     oldpeak: float
-    sTSlope: str
+    sTSlope: STSlopeEnum
 
     class Config:
         use_enum_values = True
